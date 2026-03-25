1885aebbc2274f3250f071b49f616f1884f190bc
Sien <an.nguyen009@umb.edu>
2026-03-25 01:11:49 -0400

    Update benefitsData.ts

--- src/app/data/benefitsData.ts
+++ src/app/data/benefitsData.ts
@@ -192,7 +192,7 @@ export const questions: Question[] = [
     conditions: [
       {
         questionId: 'student_status',
-        values: ['full_time', 'part_time',' future'],
+        values: ['full_time', 'part_time','future'],
       },
     ],
     options: [
