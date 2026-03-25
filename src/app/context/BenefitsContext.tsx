28d1bdfb48752d3b150a2dc48b5178642793b64c
Sien <an.nguyen009@umb.edu>
2026-03-25 01:51:03 -0400

    BenefitsContext.tsx

--- src/app/context/BenefitsContext.tsx
+++ src/app/context/BenefitsContext.tsx
@@ -113,7 +113,12 @@ const evaluateBenefitsLocally = (profile: Record<string, string>): Benefit[] => 
 
   if (
     isStudentOrFuture &&
-    (profile['transportation'] === 'yes' || profile['transportation'] === 'sometimes')
+    profile['transportation'] === 'yes' ||
+    profile['transportation'] === 'sometimes' ||
+    profile['mbta-specials'] === 'yes' ||
+    profile['mbta-disability'] === 'yes' ||
+    profile['mbta-program'] === 'yes' ||
+    profile['mbta-uni'] === 'yes'
   ) {
     const benefit = getBenefitById('mbta-pass');
     if (benefit) {
